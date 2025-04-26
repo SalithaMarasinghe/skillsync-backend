214b40a1f081649866d8eb3d18f6b47ce2f2b0a4
Thushi-2000 <thushani.kavindya2016@gmail.com>
2025-04-26 14:51:49 +0530

    Update comment.java

--- src/main/java/com/skillsync/entity/Comment.java
+++ src/main/java/com/skillsync/entity/Comment.java
@@ -5,10 +5,10 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
-@Data   //from Lombok (generates getters, setters, toString, etc.)
+@Data                               //from Lombok (generates getters, setters, toString, etc.)
 @Document(collection = "comments")  //Specifies this entity maps to a MongoDB collection named "comments"
 public class Comment {
-    @Id
+    @Id                        //Marks the field id as the Primary Key
     private String id;
     private String postId;
     private String userId;
